a40b97112a09b25049a1ef28c68adc37e9a98e17
TheAssassin <theassassin@assassinate-you.net>
2019-04-06 22:19:02 +0200

    Improve glibc API version check
    Also adds support for Ubuntu.

--- appimagelint/checks/glibc_abi.py
+++ appimagelint/checks/glibc_abi.py
@@ -9,14 +9,12 @@ import packaging.version
 from ..services import BinaryWalker
 from ..models import AppImage, TestResult
 from .._logging import make_logger
-from ..data import debian_glibc_versions_data_path, debian_codename_map_path
+from ..data import debian_glibc_versions_data_path, debian_codename_map_path, ubuntu_glibc_versions_data_path
 
 from . import CheckBase
 
 
 class GlibcABICheck(CheckBase):
-    _logger = make_logger("glibc_abi_check")
-
     def __init__(self, appimage: AppImage):
         super().__init__(appimage)
 
@@ -51,24 +49,40 @@ class GlibcABICheck(CheckBase):
     def run(self) -> Iterator[TestResult]:
         glibc_versions = set()
 
-        self._logger.info("detecting runtime's glibc version requirements")
-        glibc_versions.update(self._detect_glibc_versions(self._appimage.path()))
+        logger = self.get_logger()
 
-        self._logger.info("detecting AppImage payload's glibc version requirements")
+        glibc_versions.update(self._detect_glibc_versions(self._appimage.path()))
+        logger.info("detected required glibc version for runtime: {}".format(max(glibc_versions)))
+
         with self._appimage.mount() as mountpoint:
+            payload_glibc_versions = set()
+
             for executable in BinaryWalker(mountpoint):
                 # this check takes advantage of libc embedding static symbols into the binary depending on what
                 # features are used
                 # even binaries built on newer platforms may be running on older systems unless such features are used
                 # example: a simple hello world built on bionic can run fine on trusty just fine
-                glibc_versions = self._detect_glibc_versions(executable)
+                executable_glibc_versions = self._detect_glibc_versions(executable)
+                payload_glibc_versions.update(executable_glibc_versions)
 
-                glibc_versions.update(glibc_versions)
+            glibc_versions.update(payload_glibc_versions)
 
-            required_glibc_version = max(glibc_versions)
+            if not payload_glibc_versions:
+                logger.warning("AppImage payload does not seem to depend on glibc, this is quite unusual")
+            else:
+                logger.info("detected required glibc version for payload: {}".format(max(payload_glibc_versions)))
 
-            for result in self._check_debian_stable_compat(required_glibc_version):
-                yield result
+        if not glibc_versions:
+            raise ValueError("Could not detect dependency of runtime on glibc")
+
+        required_glibc_version = max(glibc_versions)
+        logger.debug("overall required glibc version: {}".format(required_glibc_version))
+
+        for result in self._check_debian_compat(required_glibc_version):
+            yield result
+
+        for result in self._check_ubuntu_compat(required_glibc_version):
+            yield result
 
     @classmethod
     def _get_debian_codename_map(cls):
@@ -81,7 +95,12 @@ class GlibcABICheck(CheckBase):
             return json.load(f)
 
     @classmethod
-    def _check_debian_stable_compat(cls, required_glibc: packaging.version.Version) -> Iterator[TestResult]:
+    def _get_glibc_ubuntu_versions_map(cls):
+        with open(ubuntu_glibc_versions_data_path(), "r") as f:
+            return json.load(f)
+
+    @classmethod
+    def _check_debian_compat(cls, required_glibc: packaging.version.Version) -> Iterator[TestResult]:
         codename_map = cls._get_debian_codename_map()
         glibc_versions_map = cls._get_glibc_debian_versions_map()
 
@@ -92,3 +111,11 @@ class GlibcABICheck(CheckBase):
             should_run = required_glibc <= packaging.version.parse(max_supported_glibc)
 
             yield TestResult(should_run, "AppImage can run on Debian {} ({})".format(suite, codename))
+
+    @classmethod
+    def _check_ubuntu_compat(cls, required_glibc: packaging.version.Version) -> Iterator[TestResult]:
+        glibc_versions_map = cls._get_glibc_ubuntu_versions_map()
+
+        for release, glibc_versions in glibc_versions_map.items():
+            should_run = required_glibc <= max([packaging.version.Version(v) for v in glibc_versions])
+            yield TestResult(should_run, "AppImage can run on Ubuntu {}".format(release))
