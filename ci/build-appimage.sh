8814e9a870e595dc7ca3ff5607f97017b161d688
TheAssassin <theassassin@assassinate-you.net>
2022-01-03 14:31:34 +0100

    Fix output name

--- ci/build-appimage.sh
+++ ci/build-appimage.sh
@@ -58,6 +58,8 @@ install -D "$REPO_ROOT"/resources/com.github.theassassin.appimagelint.appdata.xm
 AppDir/usr/conda/bin/python3 -m appimagelint.cache bundle_metadata
 
 # now, actually build AppImage
+# guessing the output architecture from $ARCH does not seem to work (any more?), so we specify the output name manually
+export OUTPUT=appimagelint-"$ARCH".AppImage
 ./linuxdeploy-x86_64.AppImage --appdir AppDir --output appimage
 
 # test AppImage with itself
